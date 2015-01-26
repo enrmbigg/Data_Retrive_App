652cc05681317d0778bf30f994bffefc897abc4c
Michael Biggs <mikeylfc007@blueyonder.co.uk>
2015-01-26 19:51:23 +0000

    Add Full app

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Data_Retrive_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtTable = new System.Windows.Forms.TextBox();
+            this.lblTable = new System.Windows.Forms.Label();
+            this.lblDB = new System.Windows.Forms.Label();
+            this.txtCon = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(3, 211);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(128, 23);
+            this.btnWrite.TabIndex = 12;
+            this.btnWrite.Text = "Write Folder To DB";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(3, 9);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(266, 43);
+            this.lblResult.TabIndex = 14;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(137, 211);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(132, 23);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete All Records";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtTable
+            // 
+            this.txtTable.Location = new System.Drawing.Point(137, 185);
+            this.txtTable.Name = "txtTable";
+            this.txtTable.Size = new System.Drawing.Size(100, 20);
+            this.txtTable.TabIndex = 16;
+            this.txtTable.Text = "Insert Table Here";
+            // 
+            // lblTable
+            // 
+            this.lblTable.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTable.Location = new System.Drawing.Point(3, 69);
+            this.lblTable.Name = "lblTable";
+            this.lblTable.Size = new System.Drawing.Size(266, 43);
+            this.lblTable.TabIndex = 14;
+            // 
+            // lblDB
+            // 
+            this.lblDB.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDB.Location = new System.Drawing.Point(3, 122);
+            this.lblDB.Name = "lblDB";
+            this.lblDB.Size = new System.Drawing.Size(266, 43);
+            this.lblDB.TabIndex = 14;
+            // 
+            // txtCon
+            // 
+            this.txtCon.Location = new System.Drawing.Point(31, 185);
+            this.txtCon.Name = "txtCon";
+            this.txtCon.Size = new System.Drawing.Size(100, 20);
+            this.txtCon.TabIndex = 17;
+            this.txtCon.Text = "Insert DB Con Here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 243);
+            this.Controls.Add(this.txtCon);
+            this.Controls.Add(this.txtTable);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lblDB);
+            this.Controls.Add(this.lblTable);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnWrite);
+            this.Name = "Form1";
+            this.Text = "Data App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtTable;
+        private System.Windows.Forms.Label lblTable;
+        private System.Windows.Forms.Label lblDB;
+        private System.Windows.Forms.TextBox txtCon;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Data_Retrive_App
+{
+    public partial class Form1 : Form
+    {
+        string Tour;
+        DateTime Tour_Date;
+        DateTime FileNameTime;
+        string filenameWithoutPath;
+        DateTime fileCreatedDate;
+        DateTime fileModifiedDate;
+        string table;
+        string db;
+        int i = 0;
+        System.Data.SqlClient.SqlConnectionStringBuilder builder = new System.Data.SqlClient.SqlConnectionStringBuilder();
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+            builder["Data Source"] = "localhost";
+            builder["integrated Security"] = true;
+            builder["Initial Catalog"] = "Data";
+            lblDB.Text = builder.ConnectionString;
+            
+                   
+        }
+               
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                    
+        }
+        /*
+        //Not needed atm but could be reused
+        public void DeleteLastRecord()
+        {
+            using (SqlConnection openCon = new SqlConnection(builder.ConnectionString))
+            {
+                openCon.Open();
+                string query = "DELETE FROM " + table.ToUpper() + " WHERE ID = (SELECT MAX(ID) FROM Data)";
+                SqlCommand cmd = new SqlCommand(query, openCon);
+                cmd.ExecuteNonQuery();
+                openCon.Close();
+            }
+        }
+        */
+        public void FileWrite()
+        {
+
+            using (SqlConnection openCon = new SqlConnection(builder.ConnectionString))
+            {
+                i++;
+                openCon.Open();
+                string query = "INSERT into " + table.ToUpper() + " (FileName,Tour,TourDate,FileNameTime,FileCreatedTime,FileModifiedTime) values('" + filenameWithoutPath + "','" + Tour + "','" + Tour_Date.ToString("MM/dd/yyyy") + "','" + FileNameTime.ToString("hh:mm:ss") + "','" + fileCreatedDate.ToString("MM/dd/yyyy hh:mm:ss") + "','" + fileModifiedDate.ToString("MM/dd/yyyy hh:mm:ss") + "' )";
+                SqlCommand cmd = new SqlCommand(query, openCon);
+                cmd.ExecuteNonQuery();
+                openCon.Close();
+            }
+        }
+
+        public void TextCheck()
+        {
+            try
+            {
+                if (txtTable.Text == "Insert Table Here" )
+                {
+                    table = "Info";
+                }
+                else if (txtTable.Text == "")
+                {
+                    table = "Info";
+                }
+                else
+                {
+                    table = txtTable.Text;
+                }
+
+                if (txtCon.Text == "Insert DB Con Here")
+                {
+                    db = "localhost";
+                }
+                else if (txtTable.Text == "")
+                {
+                    db = "localhost";
+                }
+                else
+                {
+                    db = txtCon.Text;
+                }
+            }
+            catch
+            {
+                table = "Info";
+                db = "localhost";
+            }
+            lblTable.Text = table;
+            builder["Data Source"] = db;
+            lblDB.Text = builder.ConnectionString;
+        }
+
+        public void FileLoad()
+        {
+           
+            FolderBrowserDialog ofd = new FolderBrowserDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                string[] fileEntries = Directory.GetFiles(ofd.SelectedPath);
+                int Total = fileEntries.Length;
+                
+                foreach (string fileName in fileEntries)
+                {
+                    lblResult.Text = "Total: " + Total.ToString() + " Amount Left: " + (Total - i).ToString();
+                    fileCreatedDate = File.GetCreationTime(fileName);
+                    filenameWithoutPath = Path.GetFileName(fileName);
+                    fileModifiedDate = File.GetLastWriteTime(fileName);
+
+                    string[] split = filenameWithoutPath.Split('_');
+                    @Tour = split[0];
+
+                    string s = split[1];
+                    string a;
+                    string b;
+                    string c;
+                    string str;
+
+                    if (s.Length ==8)
+                    {
+                        a = s.Substring(0,4);
+                        b = s.Substring(4, 2);
+                        c = s.Substring(6);
+                        str = a + '/' + b + '/' + c;
+                    }
+                    else
+                    {
+                        a = s.Substring(0, 2);
+                        b = s.Substring(2, 2);
+                        c = s.Substring(4);
+                        str = a + '/' + b + '/' + c;
+                    }
+                    Tour_Date = DateTime.Parse(str);
+                    s = split[2];
+                    string[] split1 = s.Split('.');
+                    s = split1[0];
+                    a = s.Substring(0, 2);
+                    b = s.Substring(2,2);
+                    c = s.Substring(4);
+                    str = a + ':' + b + ':' + c;
+                    try
+                    {
+                        FileNameTime = DateTime.Parse(str);
+                    }
+                    catch
+                    {
+                        FileNameTime = DateTime.Parse("00:00:00");
+                    }
+                    FileWrite();
+                    if ((i % 50) == 0)
+                    {
+                        lblResult.Text = "Total: " + Total.ToString() + " Amount Left: " + (Total - i).ToString();
+                        this.Refresh();
+                    }
+                    
+                }
+                lblResult.Text = "Total: " + Total.ToString() + " Amount Left: " + (Total - i).ToString();
+                MessageBox.Show("Saving is done!");
+                i = 0;
+            }
+        }
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            TextCheck();
+            FileLoad();
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TextCheck();
+            using (SqlConnection openCon = new SqlConnection(builder.ConnectionString))
+            {
+                i++;
+                openCon.Open();
+                string query = "DELETE " + " FROM " + table;
+                SqlCommand cmd = new SqlCommand(query, openCon);
+                cmd.ExecuteNonQuery();
+                openCon.Close();
+            }
+          
+            lblResult.Text = "Total Records: 0";
+            this.Refresh();
+            
+            MessageBox.Show("All Records Have Been Deleted!");
+        }
+    }
+}
